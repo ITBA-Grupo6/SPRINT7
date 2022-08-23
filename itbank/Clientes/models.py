13178a666a3dc3eae05682797d4c16db37eb0447
mbelenm <machenabelen@gmail.com>
2022-08-23 12:53:08 -0300

    Problematica3yCambios

--- itbank/Clientes/models.py
+++ itbank/Clientes/models.py
@@ -12,3 +12,24 @@ class Cliente(models.Model):
     class Meta:
         managed = False
         db_table = 'cliente'
+        
+
+class TiposDeCliente(models.Model):
+    tipo_id = models.AutoField(primary_key=True, blank=True)
+    nombre_tipo = models.TextField()
+    tarjetas_de_debito = models.TextField(blank=True, null=True)  
+    caja_de_ahorro_usd = models.TextField()
+    descubierto = models.IntegerField()
+    tarjetas_de_credito = models.IntegerField()
+    retiro_de_efectivo = models.IntegerField()
+    chequeras = models.IntegerField()
+    comision_por_transferencia = models.DecimalField(max_digits = 5, decimal_places = 2)
+    max_transferencias = models.IntegerField(blank=True, null=True)
+    max_prestamo = models.IntegerField(null=True, blank = True)
+
+    class Meta:
+        managed = False
+        db_table = 'tipos_de_cliente'
+    
+    def __str__(self):
+        return self.nombre_tipo
--- itbank/Cuentas/models.py
+++ itbank/Cuentas/models.py
@@ -9,3 +9,23 @@ class Cuenta(models.Model):
     class Meta:
         managed = False
         db_table = 'cuenta'
+
+
+class Movimientos(models.Model):
+    movimiento_id = models.AutoField(primary_key=True, blank=True)
+    numero_de_cuenta = models.IntegerField(blank=True, null=True)
+    monto = models.TextField(blank=True, null=True)
+    tipo_de_operacion = models.TextField(blank=True, null=True)
+    hora = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'movimientos'
+        verbose_name_plural = "Movimientos"
+
+class TiposDeCuenta(models.Model):
+    tipo_id = models.AutoField(primary_key=True, blank=True)
+    nombre_cuenta = models.TextField()
+
+    class Meta:
+        db_table = 'tipos_de_cuenta'
+
